722fbbf526a1defb5de14b2014ccf35933a901ed
A.G <admcuentas.11@gmail.com>
2023-11-07 03:45:27 -0300

    3

--- Restaurante/models.py
+++ Restaurante/models.py
@@ -4,28 +4,28 @@ from django.db import models
 class Empleado(models.Model):
     nombre = models.CharField(max_length=64)
     apellido = models.CharField(max_length=64)
-    sueldoBase = models.CharField(max_length=64)
+    sueldoBase = models.IntegerField()
     telefono = models.CharField(max_length=64)
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField(null=True, blank=True)
 
 class Reservas(models.Model):
-    numeroDeReserva = models.CharField(max_length=64)
+    numeroDeReserva = models.IntegerField()
     nombre_cliente = models.CharField(max_length=64)
-    numero_comensales = models.CharField(max_length=64)
+    numero_comensales = models.IntegerField()
 
 class Inventario(models.Model):
-    cantidad_platosFinalDia = models.CharField(max_length=64)
-    cantidad_vasosFinalDia = models.CharField(max_length=64)
-    cantidad_cuchillosFinalDia = models.CharField(max_length=64)
-    cantidad_tenedoresFinalDia = models.CharField(max_length=64)
+    cantidad_platosFinalDia = models.IntegerField()
+    cantidad_vasosFinalDia = models.IntegerField()
+    cantidad_cuchillosFinalDia = models.IntegerField()
+    cantidad_tenedoresFinalDia = models.IntegerField()
 
 class Menu(models.Model):
     nombreDelPlato = models.CharField(max_length=64)
-    precioDelPlato = models.CharField(max_length=64)
+    precioDelPlato = models.IntegerField()
     descripcionDelPlato = models.CharField(max_length=64)
 
 class Suministros(models.Model):
     nombreIngrediente = models.CharField(max_length=64)
     nombreBebidas = models.CharField(max_length=64)
-    cantidad_Bebidas = models.CharField(max_length=64)
+    cantidad_Bebidas = models.IntegerField()
